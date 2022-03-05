c22b822eda523825f893e7477512f02a51ddfe09
jaeeunjeong <jejeong000@gmail.com>
2022-03-05 20:55:22 +0900

    init

---
+++ src/main/java/me/toyproject/loginjwt/LoginJwtApplication.java
@@ -0,0 +1,13 @@
+package me.toyproject.loginjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/toyproject/loginjwt/LoginJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package me.toyproject.loginjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
