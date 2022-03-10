3f2ad0a3c12f0afcd9f6fe670c80043730addf77
jaeeunjeong <jejeong000@gmail.com>
2022-03-10 14:32:51 +0900

    feat : 회원 가입 및 권한 검증

---
+++ src/main/java/me/toyproject/loginjwt/service/UserService.java
@@ -0,0 +1,50 @@
+package me.toyproject.loginjwt.service;
+
+import me.toyproject.loginjwt.dto.UserDto;
+import me.toyproject.loginjwt.entity.Authority;
+import me.toyproject.loginjwt.entity.User;
+import me.toyproject.loginjwt.repository.UserRepository;
+import me.toyproject.loginjwt.util.SecurityUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.StringUtils;
+
+import java.util.Collections;
+import java.util.Optional;
+
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User signup(UserDto userDto) {
+
+        if(userRepository.findOneWithAuthoritiesByUsername(userDto.getUserName()).orElse(null) != null){
+            throw new RuntimeException("이미 등록되어 있는 유저입니다.");
+        }
+
+        Authority authority = Authority.builder().authorityName("ROLE_USER").build();
+
+        User user = User.builder()
+                .username(userDto.getUserName())
+                .password(passwordEncoder.encode(userDto.getPassword()))
+                .nickname(userDto.getNickname())
+                .authoritySet(Collections.singleton(authority))
+                .activated(true)
+                .build();
+
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUserWithAuthorities(String username){
+        return userRepository.findOneWithAuthoritiesByUsername(username);
+    }
+
+    public Optional<User> getMyUserWithAuthorities(){
+        return SecurityUtil.getCurrentUsername().flatMap(userRepository :: findOneWithAuthoritiesByUsername);
+    }
+}
---
+++ src/main/java/me/toyproject/loginjwt/util/SecurityUtil.java
@@ -0,0 +1,38 @@
+package me.toyproject.loginjwt.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+public class SecurityUtil {
+    private static final Logger logger = LoggerFactory.getLogger(SecurityUtil.class);
+
+    private SecurityUtil() {
+    }
+
+    /**
+     * JwtFilter클래스의 doFilter에서 저장한 Security Context의 인증 정보에서 username을 리턴
+     * @return
+     */
+    public static Optional<String> getCurrentUsername() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null) {
+            logger.debug("Security Context에 인증 정보가 없습니다.");
+            return Optional.empty();
+        }
+
+        String username = null;
+        if (authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            username = userDetails.getUsername();
+        } else if (authentication.getPrincipal() instanceof String) {
+            username = (String) authentication.getPrincipal();
+        }
+        return Optional.ofNullable(username);
+    }
+}
